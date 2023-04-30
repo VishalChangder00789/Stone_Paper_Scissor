c6f451d7cc25ceefcdc8bcb726f35e51199675c9
Bishal Changder <changdervishal1999@gmail.com>
2023-04-30 13:57:30 +0530

    Code Reworked

--- index.js
+++ index.js
@@ -1,319 +1,284 @@
-let userImage = "";
-let pcImage = "";
-// More references
-var yourChoice;
-var pcChoice;
-// CONSTANT VALUES
-const rock = "rock";
-const paper = "paper";
-const scissor = "scissor";
-// IMAGE SOURCES
-const rockImageSrc = "./Assets//Png/Rock.png";
-const paperImageSrc = "./Assets//Png/Paper.png";
-const ScissorImageSrc = "./Assets//Png/Scissors.png";
-// SCORE
+//Score
 let userScore = 0;
 let pcScore = 0;
-// SELECTION
+
+// STATE MANAGEMENT
+window.onload = function () {
+  userScore = Number(sessionStorage.getItem("userScore"));
+  pcScore = Number(sessionStorage.getItem("pcScore"));
+  updateScore();
+};
+
+// References
+let userWin = false;
+let pcWin = false;
+let tie = false;
+
+// Constant
+const rock = "Rock";
+const paper = "Paper";
+const scissor = "Scissors";
+
+// Variables
 let userSelected = "";
 let pcSelected = "";
+var userImage = "";
+var pcImage = "";
 
-//references
-let rockRef = document.getElementById("rock");
-let paperRef = document.getElementById("paper");
-let scissorRef = document.getElementById("scissor");
-let textRef = document.getElementById("text");
-let playAgainButton = document.getElementById("playAgainButton");
-let youWinRef = document.getElementById("you-win");
-let pcWinRef = document.getElementById("pc-win");
-let tieRef = document.getElementById("tie");
-let crossButtonRef = document.getElementById("cross");
-let rulesBoxRef = document.getElementById("rules-box");
-let rulesButtonRef = document.getElementById("rulesButton");
-let userScoreRef = document.getElementById("user-score");
-let compScoreRef = document.getElementById("comp-score");
-let nextButtonRef = document.getElementById("nextButton");
-let victoriusRef = document.getElementById("vic-id");
-let scoreContainerRef = document.getElementById("score-container-id");
-let userContainerRef = document.getElementById("userContainer-id");
+//Score
+const userScore_Ref = document.getElementById("user-score-id");
+const pcScore_Ref = document.getElementById("pc-score-id");
+const scoreContainer_Ref = document.getElementById("score-container-id");
 
-// INITIALIZATION
+// Triangle
+const userContainer_Ref = document.getElementById("userContainer-id"); // for triangle
 
-victoriusRef.style.display = "none";
-youWinRef.style.display = "none";
-pcWinRef.style.display = "none";
-tieRef.style.display = "none";
-playAgainButton.style.display = "none";
+// You pick pc pick
+const youPickPcPick_Ref = document.getElementById(
+  "selectionDisplayContainer-id"
+);
+
+const deiciderContainer = document.getElementById("DeciderContainer-id");
+
+// Rules Box
+const rulesBox_Ref = document.getElementById("rulesBox-id");
+const crossButton_Ref = document.getElementById("cross-id");
+const rulesButton_Ref = document.getElementById("rulesButton");
+const nextButton_Ref = document.getElementById("nextButton");
+
+// Rules button and Next Button togather
+const combinedButton_Ref = document.getElementById("AllButtons");
+
+// Individual Elements
+const youWin_Ref = document.getElementById("you-win");
+const pcWin_Ref = document.getElementById("pc-win");
+const tie_Ref = document.getElementById("tie");
+const playAgainButton_Ref = document.getElementById("playAgainButton");
+
+const rockButton_Ref = document.getElementById("rock");
+const paperButton_Ref = document.getElementById("paper");
+const scissorButton_Ref = document.getElementById("scissor");
+
+const victorius_Ref = document.getElementById("vic-id");
+const playAgainVictory_Ref = document.getElementById("playAgainButton-2");
+
+victorius_Ref.style.display = "none";
+
+// INITIALISATION
+deiciderContainer.style.display = "none";
+youWin_Ref.style.display = "none";
+pcWin_Ref.style.display = "none";
+tie_Ref.style.display = "none";
+playAgainButton_Ref.style.display = "none";
+youPickPcPick_Ref.style.display = "none";
+
+if (userScore > pcScore) {
+  nextButton_Ref.style.display = "block";
+} else {
+  nextButton_Ref.style.display = "none";
+}
 
 // CLICK EVENT LISTNERS
-
-nextButtonRef.addEventListener("click", (e) => {
-  console.log("Next Button Clicked");
-  nextButtonController();
+rockButton_Ref.addEventListener("click", (e) => {
+  // console.log("Rock Clicked");
+  HandleUserChoiceButton(0);
 });
 
-rulesButtonRef.addEventListener("click", (e) => {
+paperButton_Ref.addEventListener("click", (e) => {
+  console.log("Paper Clicked");
+  HandleUserChoiceButton(1);
+});
+
+scissorButton_Ref.addEventListener("click", (e) => {
+  // console.log("scissor Clicked");
+  HandleUserChoiceButton(2);
+});
+
+crossButton_Ref.addEventListener("click", (e) => {
+  closeRulesBox();
+});
+
+rulesButton_Ref.addEventListener("click", (e) => {
   openRulesBox();
 });
 
-crossButtonRef.addEventListener("click", (e) => {
-  closeRulesBox(e);
+playAgainButton_Ref.addEventListener("click", (e) => {
+  HandlePlayAgain();
 });
 
-rockRef.addEventListener("click", (e) => {
-  handleOptionClick(e, 0);
+playAgainVictory_Ref.addEventListener("click", (e) => {
+  HandlePlayAgain2();
 });
-
-paperRef.addEventListener("click", (e) => {
-  handleOptionClick(e, 1);
+nextButton_Ref.addEventListener("click", (e) => {
+  HandleNextButton();
 });
+// CLICK EVENT LISTNERS
 
-scissorRef.addEventListener("click", (e) => {
-  handleOptionClick(e, 2);
-});
-
-playAgainButton.addEventListener("click", (e) => {
-  playAgain(e);
-});
-
-// HANDLES CLICK ON ANY OF THE THREE OPTIONS
-
-function handleOptionClick(e, valuePassed) {
-  if (valuePassed === 0) {
-    disableCss();
-    userImage = instantiatePng(
-      rockImageSrc,
-      100,
-      100,
-      "absolute",
-      "35%",
-      "45%",
-      rock
-    );
-    userSelected = rock;
-    pcSelected = generateRandom();
-    pcImage = instantiator(pcSelected);
-  } else if (valuePassed === 1) {
-    userSelected = paper;
-    disableCss();
-    userImage = instantiatePng(
-      paperImageSrc,
-      100,
-      100,
-      "absolute",
-      "35%",
-      "45%",
-      paper
-    );
-
-    pcSelected = generateRandom();
-    pcImage = instantiator(pcSelected);
-  } else if (valuePassed == 2) {
-    userSelected = scissor;
-    disableCss();
-    userImage = instantiatePng(
-      ScissorImageSrc,
-      100,
-      100,
-      "absolute",
-      "35%",
-      "45%",
-      scissor
-    );
-    pcSelected = generateRandom();
-    pcImage = instantiator(pcSelected);
-  }
-  showBanner();
-  document.body.appendChild(userImage);
-  document.body.appendChild(pcImage);
-  playAgainButton.style.display = "inline-block";
-  whoWon(userSelected, pcSelected);
+function randomGenerator() {
+  const choice = [rock, paper, scissor];
+  return choice[Math.floor(Math.random() * choice.length)];
 }
 
-// Return an image Instantiate an Image on the Go
-function instantiatePng(src, width, height, position, left, top, options) {
-  let x = document.createElement("IMG");
-
-  if (options == rock) {
-    x.style.boxShadow = "0 0 0px 10px rgb(0, 21, 255)";
-  } else if (options == paper) {
-    x.style.boxShadow = "0 0 0px 10px rgb(255, 128, 0)";
-  } else if (options == scissor) {
-    x.style.boxShadow = "0 0 0px 10px rgb(166, 0, 255)";
-  }
-
-  // Attributes
-  x.setAttribute("src", `${src}`);
-  x.setAttribute("width", `${width}`);
-  x.setAttribute("height", `${height}`);
-
-  //Styling
-  x.style.position = `${position}`;
-  x.style.left = `${left}`;
-  x.style.top = `${top}`;
-
-  x.className = "image-beauty";
-
-  // Sending
-  return x;
-  // document.body.appendChild(x);
-}
-
-// Generate Random Choice for Computer
-function generateRandom() {
-  const options = ["rock", "paper", "scissor"];
-  return options[Math.floor(Math.random() * options.length)];
-}
-
-// Custom Choice for computer
-function instantiator(pcSelected) {
-  if (pcSelected == "rock") {
-    return instantiatePng(
-      rockImageSrc,
-      100,
-      100,
-      "absolute",
-      "55%",
-      "45%",
-      rock
-    );
-  } else if (pcSelected == "paper") {
-    return instantiatePng(
-      paperImageSrc,
-      100,
-      100,
-      "absolute",
-      "55%",
-      "45%",
-      paper
-    );
-  } else if (pcSelected == "scissor") {
-    return instantiatePng(
-      ScissorImageSrc,
-      100,
-      100,
-      "absolute",
-      "55%",
-      "45%",
-      scissor
-    );
-  }
-}
-
-// Disable the required CSS
-function disableCss() {
-  document.getElementById("userContainer-id").style.display = "none";
-}
-
-function showBanner() {
-  yourChoice = document.createElement("div");
-  pcChoice = document.createElement("div");
-  yourChoice.innerHTML = "Your Choice";
-  pcChoice.innerHTML = "Pc Choice";
-
-  // STYLING
-  yourChoice.style.display = "inline";
-  yourChoice.style.position = "absolute";
-  yourChoice.style.top = "38%";
-  yourChoice.style.left = "33%";
-
-  pcChoice.style.display = "inline";
-  pcChoice.style.position = "absolute";
-  pcChoice.style.top = "38%";
-  pcChoice.style.left = "55%";
-
-  yourChoice.classList.add("text");
-  pcChoice.classList.add("text");
-
-  document.body.appendChild(yourChoice);
-  document.body.appendChild(pcChoice);
-}
-
-function playAgain() {
-  userImage.style.display = "none";
-  pcImage.style.display = "none";
-
-  document.getElementById("userContainer-id").style.display = "block";
-  yourChoice.style.display = "none";
-  pcChoice.style.display = "none";
-  playAgainButton.style.display = "none";
-
-  youWinRef.style.display = "none";
-  pcWinRef.style.display = "none";
-  tieRef.style.display = "none";
-}
-
-function whoWon(userSelected, pcSelected) {
-  if (userSelected == rock && pcSelected == paper) {
-    // PC WON
-    pcWinRef.style.display = "block";
-
-    // Add Ripple
-    pcImage.classList.add("winRipple");
-
-    pcScore += 5;
-  } else if (userSelected == rock && pcSelected == scissor) {
-    // USER WON
-    userImage.classList.add("winRipple");
-    youWinRef.style.display = "block";
-    userScore += 5;
-  } else if (userSelected == scissor && pcSelected == paper) {
-    //USER WON
-    userScore += 5;
-    userImage.classList.add("winRipple");
-
-    youWinRef.style.display = "block";
-  } else if (userSelected == scissor && pcSelected == rock) {
-    //PC WON
-    pcScore += 5;
-    pcImage.classList.add("winRipple");
-
-    pcWinRef.style.display = "block";
-  } else if (userSelected == paper && pcSelected == scissor) {
-    // PC WON
-    pcScore += 5;
-    pcImage.classList.add("winRipple");
-    pcWinRef.style.display = "block";
-  } else if (userSelected == paper && pcSelected == rock) {
-    // USER WON
-    userScore += 5;
-    userImage.classList.add("winRipple");
-    youWinRef.style.display = "block";
-  } else if (userSelected == pcSelected) {
-    //TIE
-    tieRef.style.display = "block";
-  }
-
-  userScoreRef.innerHTML = userScore;
-  compScoreRef.innerHTML = pcScore;
+function DisableCss() {
+  // Disappear the triangle
+  userContainer_Ref.style.display = "none";
 }
 
 function closeRulesBox() {
-  rulesBoxRef.style.display = "none";
+  rulesBox_Ref.style.display = "none";
 }
 
 function openRulesBox() {
-  rulesBoxRef.style.display = "inline-block";
+  rulesBox_Ref.style.display = "inline-block";
+}
+/*
+
+
+
+
+*/
+function updateScore() {
+  userScore_Ref.innerHTML = userScore;
+  pcScore_Ref.innerHTML = pcScore;
+}
+function InstantiateImage(whoHasSelectedWhat, whoHasPassed) {
+  let Source = `./Assets//Png2/${whoHasSelectedWhat}.png`;
+  let el = document.createElement("IMG");
+
+  // Styling
+  el.className = "img-dimension";
+  if (whoHasSelectedWhat == rock) {
+    el.id = "rock";
+  } else if (whoHasSelectedWhat == paper) {
+    el.id = "paper";
+  } else if (whoHasSelectedWhat == scissor) {
+    el.id = "scissor";
+  }
+
+  el.setAttribute("src", Source);
+  el.style.position = "absolute";
+
+  if (whoHasPassed == "user") {
+    // Fix the dimension
+    console.log("user sent");
+    el.style.top = "40%";
+    el.style.left = "23%";
+  } else if (whoHasPassed == "pc") {
+    console.log("pc sent");
+    el.style.top = "40%";
+    el.style.left = "58%";
+  }
+
+  return el;
+}
+function WhatBeatsWhat(userSelected, pcSelected) {
+  // user won cases
+  if (
+    (userSelected == rock && pcSelected == scissor) ||
+    (userSelected == paper && pcSelected == rock) ||
+    (userSelected == scissor && pcSelected == paper)
+  ) {
+    console.log("User Wins");
+    userWin = true;
+    userScore += 5;
+  } else if (userSelected == pcSelected) {
+    console.log("Tie");
+    tie = true;
+  } else {
+    console.log("Pc Wins");
+    pcWin = true;
+    pcScore += 5;
+  }
+  sessionStorage.setItem("userScore", userScore);
+  sessionStorage.setItem("pcScore", pcScore);
+  updateScore();
+  if (userScore > pcScore) {
+    nextButton_Ref.style.display = "block";
+  } else {
+    nextButton_Ref.style.display = "none";
+  }
+  return;
+}
+function WhoWon() {
+  deiciderContainer.style.display = "inline-block";
+
+  if (userWin) {
+    youWin_Ref.style.display = "block";
+    userWin = false;
+  } else if (pcWin) {
+    pcWin_Ref.style.display = "block";
+    pcWin = false;
+  } else {
+    tie_Ref.style.display = "block";
+  }
+
+  playAgainButton_Ref.style.display = "block";
+}
+function HandleUserChoiceButton(param) {
+  if (param == 0) {
+    userSelected = rock;
+  } else if (param == 1) {
+    userSelected = paper;
+  } else if (param == 2) {
+    userSelected = scissor;
+  }
+
+  youPickPcPick_Ref.style.display = "inline-block";
+  pcSelected = randomGenerator();
+  // Creating Source
+  userImage = InstantiateImage(userSelected, "user");
+  pcImage = InstantiateImage(pcSelected, "pc");
+
+  document.body.appendChild(userImage);
+  document.body.appendChild(pcImage);
+  console.log("User Selected : ", userSelected);
+  console.log("Pc Selected : ", pcSelected);
+  // Disable all the Css
+  DisableCss();
+  WhatBeatsWhat(userSelected, pcSelected);
+  WhoWon();
 }
 
-function nextButtonController() {
-  if (userScore > pcScore) {
-    victoriusRef.style.display = "block";
+function HandlePlayAgain() {
+  // Activate the triangle
+  userContainer_Ref.style.display = "inline-block";
+  userImage.style.display = "none";
+  pcImage.style.display = "none";
+  youPickPcPick_Ref.style.display = "none";
 
-    // Hide Everthing
-    scoreContainerRef.style.display = "none";
-    userContainerRef.style.display = "none";
+  tie_Ref.style.display = "none";
+  youWin_Ref.style.display = "none";
+  pcWin_Ref.style.display = "none";
+  playAgainButton_Ref.style.display = "none";
+  deiciderContainer.style.display = "none";
 
-    yourChoice.style.display = "none";
-    pcChoice.style.display = "none";
-    youWinRef.style.display = "none";
-    playAgainButton.style.display = "none";
+  // Hide Everything else
+}
 
-    userImage.style.display = "none";
-    pcImage.style.display = "none";
+function HandlePlayAgain2() {
+  // Activate the triangle
+  userContainer_Ref.style.display = "inline-block";
+  scoreContainer_Ref.style.display = "block";
+  victorius_Ref.style.display = "none";
+  openRulesBox();
+  combinedButton_Ref.style.display = "inline-block";
 
-    rulesBoxRef.style.display = "none";
-    rulesButtonRef.style.display = "none";
-    nextButtonRef.style.display = "none";
-  }
+  // Hide Everything else
+}
+function HandleNextButton() {
+  scoreContainer_Ref.style.display = "none";
+  userContainer_Ref.style.display = "none";
+  userImage.style.display = "none";
+  pcImage.style.display = "none";
+  youPickPcPick_Ref.style.display = "none";
+  tie_Ref.style.display = "none";
+  youWin_Ref.style.display = "none";
+  pcWin_Ref.style.display = "none";
+  playAgainButton_Ref.style.display = "none";
+  deiciderContainer.style.display = "none";
+  closeRulesBox();
+  combinedButton_Ref.style.display = "none";
+
+  victorius_Ref.style.display = "inline-block";
+  // Display Victory
 }
